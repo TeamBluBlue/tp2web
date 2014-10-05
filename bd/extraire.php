f347248983a09eb59df692a639baf4d9971dda85
Charles-André Beaudry <ca_beaudry@yahoo.ca>
2014-10-05 15:37:35 -0400

    Créé le foreach pour séparé les différents placemarks. Le fichier se load

--- bd/extraire.php
+++ bd/extraire.php
@@ -1,3 +1,9 @@
 <?php 
-	echo "Ça marche";
+$xml = simplexml_load_file("http://donnees.ville.quebec.qc.ca/Handler.ashx?id=29&f=KML");
+echo "INSERT INTO blahblah () VALUES"
+foreach($xml->Document->Folder->Placemark as $placemark)
+{
+	echo "(".$placemark);
+	echo "<br />";
+}
 ?>
