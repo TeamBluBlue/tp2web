f8a3927219b88c7f2c2d15e5868d63f530799fdc
Charles-André Beaudry <ca_beaudry@yahoo.ca>
2014-10-06 15:52:11 -0400

    Continué le fichier d'insertion des données de départ

--- bd/extraire.php
+++ bd/extraire.php
@@ -1,9 +1,29 @@
-<?php 
+<?php
+header("Content-type: text/plain");
 $xml = simplexml_load_file("http://donnees.ville.quebec.qc.ca/Handler.ashx?id=29&f=KML");
-echo "INSERT INTO blahblah () VALUES"
+foreach ($xml->Document->Folder->Placemark->children() as $key => $placemark)
+{
+	var_dump($key);
+	foreach ($placemark as $key2 => $enfant) {
+		echo "\t";
+		var_dump($key2);
+		foreach ($enfant as $key3 => $bebe) {
+			echo "\t\t";
+			var_dump($bebe->attributes()[0]);
+		}
+	}
+	echo "\n";
+}
+echo "CREATE TABLE IF NOT EXISTS `Zap`(
+
+);
+
+";
+$bdd->prepare("INSERT INTO `Zap` (name,snippet,description,) VALUES ()";
 foreach($xml->Document->Folder->Placemark as $placemark)
 {
-	echo "(".$placemark);
-	echo "<br />";
+	echo "('".$placemark->name."','".$placemark->snippet."','".$placemark->description."','";
+	echo "),
+";
 }
 ?>
