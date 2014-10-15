d5dddc9b23677d71b8b24ed1f63348565a3fbcd1
Éric Bonin <eric.bonin.87@gmail.com>
2014-10-15 00:59:39 -0400

    Ajout du marqueur du client

--- js/carte-google.js
+++ js/carte-google.js
@@ -7,7 +7,7 @@ var carte;
 function googleMaps(){
 	var defAlt = 11;
 	var optionsCarte = {
-		"zoom": 14,
+		"zoom": defAlt,
 		"mapTypeId": google.maps.MapTypeId.ROADMAP
 	};
 	carte = new google.maps.Map(document.getElementById("googlemaps"), optionsCarte);
@@ -22,23 +22,70 @@ function googleMaps(){
 	else{
 		console.log('Le navigateur NE supporte PAS la géolocalisation.');
 		var positionInitiale = defPosition;
-		carte.setCenter(positionInitiale);
+		
+		// Définir le marqueur de la position du client
+		definirPositionClient(positionInitiale);
 	}
 }
 
-// Position du client obtenue
+// Reçoit la position obtenue lors la géolocalisation
+// Paramètre "position" : représente les coordonnées de la position obtenue
 function positionClient(position){
 	console.log('Position obtenue : ' + position.coords.latitude + ', ' + position.coords.longitude);
 	var positionInitiale = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	carte.setCenter(positionInitiale);
+	
+	// Définir le marqueur de la position du client
+	definirPositionClient(positionInitiale);
 }
 
 // Postion du client n'a pas été obtenue, utilisation d'une position par défaut
+// Paramètre "erreur" : représente l'erreur levée lors de l'obtention de la position
+// du client
 function positionClientErreur(erreur){
 	console.log('Utilisation de la position par défaut.');
 	var positionInitiale = defPosition;
-	carte.setCenter(positionInitiale);
+	
+	// Définir le marqueur de la position du client
+	definirPositionClient(positionInitiale);
 }
+
+// Permet de définir la position du client
+// Paramètre position : représente la position du client
+function definirPositionClient(position){
+	// Définir le marqueur de la position du client
+	var optionsRepere = {
+		"position": position, 
+		"map": carte,
+		"icon": "images/smiley-happy.png",
+		"clickable": true,
+		"title": "Vous êtes ici!"
+	};
+	var repere = new google.maps.Marker(optionsRepere);
+	
+	google.maps.event.addListener(repere, "click", function() { clicReperePosition(repere); });
+	
+	carte.setCenter(position);
+}
+
+// Permet d'ouvrir une bulle d'information au-dessus de la position du client
+// Paramètre "repere" : représente la position du client
+function clicReperePosition(repere){
+	var bullePositionClient = new google.maps.InfoWindow({
+		content: repere.getTitle()
+	});
+	bullePositionClient.open(carte, repere);
+	
+	// Centrer la carte sur le repère
+	carte.panTo(repere.getPosition());
+}
+
+// Permet de vérifier si une bulle d'information est déjà ouverte
+// Paramètre "infoWindow" = représente l'InfoWindow dont nous voulons vérifier
+// l'état d'ouverture
+//function bulleInfoOuverte(infoWindow){
+//	var carteInfoWindow = infoWindow.getMap();
+//	return (carteInfoWindow !== null && typeof carteInfoWindow !== "undefined");
+//}
 
 // Affichage de la carte Google
 google.maps.event.addDomListener(window, 'load', googleMaps);
