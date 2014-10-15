8357b86915c86a7a67deb8d8e294ee83cf1593d4
Éric Bonin <eric.bonin.87@gmail.com>
2014-10-14 23:22:18 -0400

    Ajout de la géolocalisation automatique

--- js/carte-google.js
+++ js/carte-google.js
@@ -1,17 +1,43 @@
+// Variables globales
+var defLat = 46.8380946;
+var defLong = -71.3079991;
+var defPosition = new google.maps.LatLng(defLat, defLong);
+var carte;
+
 function googleMaps(){
-	var defLat = 46.8380946;
-	var defLong = -71.3079991;
 	var defAlt = 11;
-	
-	var defPosition = new google.maps.LatLng(defLat, defLong);
 	var optionsCarte = {
-		"zoom": defAlt,
-		"center": defPosition,
-		"disableDoubleClickZoom": true,
+		"zoom": 14,
 		"mapTypeId": google.maps.MapTypeId.ROADMAP
 	};
+	carte = new google.maps.Map(document.getElementById("googlemaps"), optionsCarte);
 	
-	var carte = new google.maps.Map(document.getElementById("googlemaps"), optionsCarte);
+	
+	// Tenter de récupérer la position du client
+	if (typeof navigator.geolocation !== "undefined"){
+		console.log('Le navigateur supporte la géolocalisation.');
+		navigator.geolocation.getCurrentPosition(positionClient, positionClientErreur, {});
+	}
+	// En cas d'échec, mettre une position par défaut
+	else{
+		console.log('Le navigateur NE supporte PAS la géolocalisation.');
+		var positionInitiale = defPosition;
+		carte.setCenter(positionInitiale);
+	}
+}
+
+// Position du client obtenue
+function positionClient(position){
+	console.log('Position obtenue : ' + position.coords.latitude + ', ' + position.coords.longitude);
+	var positionInitiale = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	carte.setCenter(positionInitiale);
+}
+
+// Postion du client n'a pas été obtenue, utilisation d'une position par défaut
+function positionClientErreur(erreur){
+	console.log('Utilisation de la position par défaut.');
+	var positionInitiale = defPosition;
+	carte.setCenter(positionInitiale);
 }
 
 // Affichage de la carte Google
